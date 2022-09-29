d3e6573261f348901589db6d3812aecf684994f1
yeonwoo-Kim <yeon.8d@gmail.com>
2022-09-30 00:54:11 +0900

    Main

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
+import AlgorithmCode.*;
 public class Main {
     public static void main(String[] args) {
         /*
             선택 정렬 (SelectSort)
+            AlgorithmCode.SelectSort sort = new SelectSort();
+            sort.selectSort();
          */
-        SelectSort sort = new SelectSort();
-        sort.selectSort();
+
     }
 }
