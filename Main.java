9c2cdf29d10cb76bc12065a4b0886f47ecb589c9
yeonwoo-Kim <yeon.8d@gmail.com>
2022-10-07 00:17:13 +0900

    Main

--- Main.java
+++ Main.java
@@ -2,22 +2,23 @@ import AlgorithmCode.*;
 public class Main {
     public static void main(String[] args) {
         /*
-            선택 정렬 (SelectSort)
-            AlgorithmCode.SelectSort sort = new SelectSort();
-            sort.selectSort();
+        선택 정렬 (SelectSort)
+        AlgorithmCode.SelectSort sort = new SelectSort();
+        sort.selectSort();
          */
 
         /*
-            버블 정렬 (BubbleSort)
-            AlgorithmCode.BubbleSort sort = new BubbleSort();
-            sort.bubbleSort();
+        버블 정렬 (BubbleSort)
+        AlgorithmCode.BubbleSort sort = new BubbleSort();
+        sort.bubbleSort();
          */
 
         /*
-            삽입 정렬 (InsertSort)
-
-         */
+        삽입 정렬 (InsertSort)
         AlgorithmCode.InsertSort sort = new InsertSort();
         sort.insertSort();
+         */
+        AlgorithmCode.Greedy g = new Greedy();
+        g.greedy();
     }
 }
