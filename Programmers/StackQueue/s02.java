2073f6ef68c26408d0661dd1a62cab2cbaa6afa8
yeonwoo-Kim <yeon.8d@gmail.com>
2022-10-18 21:58:46 +0900

    프로그래머스 코딩테스트 > 스택/큐 (Lv.2 기능개발 코드개선)

--- Programmers/StackQueue/s02.java
+++ Programmers/StackQueue/s02.java
@@ -5,64 +5,46 @@ import java.util.ArrayList;
 public class s02 {
     static int[] solution(int[] progresses, int[] speeds) {
         /*
-        1. progresses의 각 원소를 확인한다
-        2. 첫번째 원소가 100이 될 때 까지 각 +speeds한다.
-        3. 각 날짜를 count++한다.
-        4. 첫번째 원소가 100이 되면 다른 원소 중 100이 된 원소를 확인한다.
-        5. 카운트된 날짜를 배열에 넣어준다.
-        6. 100이 되지 않은 원소를 100이 될 때까지 카운트한다.
-        7. 원소가 100이 되면 100이 된 원소의 수를 배열에 넣어준다.
+        [기능개발]
          */
-
         int[] answer = {};
-        int day = 0; // 날짜카운트
         ArrayList<Integer> answerList = new ArrayList<>();
-        ArrayList<Integer> q = new ArrayList<>();
-//        Queue<Integer> q = new LinkedList<>();
+        // chk: 배포된 기능(원소) 체크 (true: 배포 / false: 미배포)
+        boolean[] chk = new boolean[progresses.length];
 
-        for (int i : progresses) {
-            q.add(i);
-        }
-
-        for (int i = 0; i < q.size(); i++) {
-
-            while (q.get(i).intValue() < 100) {
-                for (int j = 0; j < q.size(); j++) {
-                    progresses[j] += speeds[j];
-                    q.set(j, progresses[j] + speeds[j]);
+        for (int i = 0; i < progresses.length; i++) {
+            // 배포되지 않은 기능(원소) 기준으로 진행 (false)
+            if (chk[i] == false) {
+                // 작업 진도가 100이 될 때 까지 각 +speeds
+                while (progresses[i] < 100) {
+                    for (int j = 0; j < progresses.length; j++) {
+                        progresses[j] += speeds[j];
+                    }
                 }
-                day++;
-            }
 
-            int cnt = 0;
-            for (int k = 0; k < progresses.length; k++) {
-                if (q.get(k) >= 100) {
-                    cnt++;
+                // 첫번째 배포되는 기능(원소) 체크
+                int cnt = 1;
+                chk[i] = true;
+
+                // 100이 된 기능(원소) 다음부터 차례로 작업진도 100 체크
+                for (int k = i + 1; k < progresses.length; k++) {
+                    // 작업진도가 100(이상)이며 배포되지 않은 기능이면
+                    if (progresses[k] >= 100 && chk[k] != true) {
+                        // 배포될 기능 개수 추가하고 배포되었다는 체크 표시
+                        cnt++;
+                        chk[k] = true;
+                    } else {
+                        break;
+                    }
                 }
+                // 총 배포될 개수 순차적 추가
+                answerList.add(cnt);
             }
-            answerList.add(cnt);
         }
-//        for(int i = 0; i<progresses.length; i++) {
-//
-//            while (progresses[i] < 100) {
-//                for (int j = 0; j < progresses.length; j++) {
-//                    progresses[j] += speeds[j];
-//                }
-//                day++;
-//            }
-//
-//            int cnt = 0;
-//            for (int k = 0; k < progresses.length; k++) {
-//                if (progresses[k] >= 100) {
-//                    cnt++;
-//                }
-//            }
-//            answerList.add(cnt);
-//        }
 
         answer = new int[answerList.size()];
-        for (int r = 0; r < answerList.size(); r++) {
-            answer[r] = answerList.get(r);
+        for (int l = 0; l < answerList.size(); l++) {
+            answer[l] = answerList.get(l);
         }
 
         for (int ii : answer) {
@@ -73,10 +55,10 @@ public class s02 {
 
 
     public static void main(String[] args) {
-        int[] arr1 = {93, 30, 55};
-        int[] arr2 = {1, 30, 5};
-//        int[] arr1 = {95, 90, 99, 99, 80, 99};
-//        int[] arr2 = {1, 1, 1, 1, 1, 1};
+//        int[] arr1 = {93, 30, 55};
+//        int[] arr2 = {1, 30, 5};
+        int[] arr1 = {95, 90, 99, 99, 80, 99};
+        int[] arr2 = {1, 1, 1, 1, 1, 1};
 
         solution(arr1, arr2);
     }
